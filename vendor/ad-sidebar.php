6dd5783f093f5e8ade47e35d670a84fa41cafd39
Michael Beckwith <tw2113@gmail.com>
2016-04-29 20:55:22 -0500

    fix duplicate function stuff

--- vendor/ad-sidebar.php
+++ vendor/ad-sidebar.php
@@ -20,49 +20,33 @@ if ( ! function_exists( 'bpextender_products_sidebar' ) ) {
 	 */
 	function bpextender_products_sidebar() {
 		if ( false === ( $ads = get_transient( 'pluginize_promos' ) ) ) {
-			$ads = wp_remote_get( 'https://pluginize.com/assets/json/sidebar.json' );
-
-			/**
-			 * Displays WebDevStudios products in a sidebar.
-			 * We hope you don't mind.
-			 *
-			 * @since 1.0.0
-			 *
-			 * @internal
-			 */
-			function bpextender_products_sidebar() {
-				if ( false === ( $ads = get_transient( 'pluginize_promos' ) ) ) {
-					$ads = wp_remote_get( 'https://pluginize.com/assets/json/sidebar.json' );
-
-					if ( 200 === wp_remote_retrieve_response_code( $ads ) ) {
-						$ads = json_decode( wp_remote_retrieve_body( $ads ) );
-						set_transient( 'pluginize_promos', $ads, DAY_IN_SECONDS );
-					}
-				}
-
-				if ( ! empty( $ads ) ) {
-					echo '<div class="pluginizepromos">';
-					foreach ( $ads->buddyextender->sidebar as $ad ) {
-						$the_ad = $ad->text;
-						$image  = wp_remote_get( $ad->image );
-						if ( 200 === wp_remote_retrieve_response_code( $image ) ) {
-							$the_ad = sprintf(
-								'<img src="%s" alt="%s">',
-								$ad->image,
-								$ad->text
-							);
-						}
-
-						printf(
-							'<a href="%s">%s</a>',
-							$ad->url,
-							$the_ad
-						);
-					}
-					echo '</div>';
-
-				}
+			if ( 200 === wp_remote_retrieve_response_code( $ads ) ) {
+				$ads = json_decode( wp_remote_retrieve_body( $ads ) );
+				set_transient( 'pluginize_promos', $ads, DAY_IN_SECONDS );
 			}
+		}
+
+		if ( ! empty( $ads ) ) {
+			echo '<div class="pluginizepromos">';
+			foreach ( $ads->buddyextender->sidebar as $ad ) {
+				$the_ad = $ad->text;
+				$image  = wp_remote_get( $ad->image );
+				if ( 200 === wp_remote_retrieve_response_code( $image ) ) {
+					$the_ad = sprintf(
+						'<img src="%s" alt="%s">',
+						$ad->image,
+						$ad->text
+					);
+				}
+
+				printf(
+					'<a href="%s">%s</a>',
+					$ad->url,
+					$the_ad
+				);
+			}
+			echo '</div>';
+
 		}
 	}
 }
